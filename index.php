8596aef0eca14ff4e8c25ed1d2553adffd69e5b2
c3to <c3to@web.de>
2018-04-16 13:45:08 +0200

    added filer

--- index.php
+++ index.php
@@ -7,7 +7,16 @@
 <body>
 
   <h1>Drupal Monitor</h1>
-  <div id="menu"></div>
+  <form id="filter" method="get">
+    <h2>Filter</h2>
+    <label for="log-files">File:</label>
+    <select name="file" id="log-files">
+      <option value="none">none</option>
+    </select>
+    <label for="type">Type:</label>
+    <input type="text" name="type" id="type" placeholder="content for example" />
+    <input type="submit" value="Submit">
+  </form>
   <div id="monitor"></div>
 
   <script type="text/javascript" src="script.js"></script>
--- script.js
+++ script.js
@@ -1,144 +1,140 @@
+( () => {
 
-// build menu links and write to #menu div
-function buildMenu(names) {
-  // build menu links
-  let menu = document.createElement('UL');
-  menu.classList.add('main-menu');
-  names.forEach( (item, index) => {
-    let li = document.createElement('LI');
-    let a = document.createElement('A');
-    a.innerText = item;
-    a.href = '?site=' + item;
-    li.appendChild(a);
-    menu.appendChild(li);
-  });
+  function scanFolder(logDir, callback) {
+    let fileextension = ".json";
 
-  // write menu
-  let container = document.getElementById('menu');
-  container.appendChild(menu);
-}
-
-function getSearchTerm() {
-  if(window.location.search.includes('?site=')) {
-    return term = window.location.search.split('?site=')[1];
+    let xobj = new XMLHttpRequest();
+        // xobj.overrideMimeType("html");
+    xobj.open('GET', logDir, true);
+    xobj.onreadystatechange = function () {
+      if (xobj.readyState == 4 && xobj.status == "200") {
+        // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+        callback(xobj.responseText);
+      }
+    };
+    xobj.send(null);
   }
-}
 
-function scanFolder(logDir, callback) {
-  let fileextension = ".json";
-
-  let xobj = new XMLHttpRequest();
-      // xobj.overrideMimeType("html");
-  xobj.open('GET', logDir, true);
-  xobj.onreadystatechange = function () {
-    if (xobj.readyState == 4 && xobj.status == "200") {
-      // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-      callback(xobj.responseText);
-    }
-  };
-  xobj.send(null);
-}
-
-function loadJSON(file, callback) {
-  let xobj = new XMLHttpRequest();
-      xobj.overrideMimeType("application/json");
-  xobj.open('GET', file, true);
-  xobj.onreadystatechange = function () {
-    if (xobj.readyState == 4 && xobj.status == "200") {
-      // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-      callback(xobj.responseText);
-    }
-  };
-  xobj.send(null);
-}
-
-function printLog(file) {
-  let table = document.createElement("TABLE");
-
-  loadJSON(file, function(response) {
-    // Parse JSON string into object
-    let logObj = JSON.parse(response);
-
-    // bild table header from the first object set
-    let thead = document.createElement("THEAD");
-    let theadRow = document.createElement("TR");
-    let firstKey = Object.keys(logObj)[0];
-    for (let key in logObj[firstKey]) {
-      let theadCell = document.createElement("TH");
-      theadCell.innerText = key;
-      theadRow.appendChild(theadCell);
-    }
-    thead.appendChild(theadRow);
-    table.appendChild(thead);
-
-    // build table body
-    let tbody = document.createElement("TBODY");
-    for (let serial in logObj) {
-      let row = document.createElement("TR");
-      row.classList.add(logObj[serial].severity);
-      for (let field in logObj[serial]) {
-        let cell = document.createElement("TD");
-        cell.innerText = logObj[serial][field];
-        row.appendChild(cell);
+  function loadJSON(file, callback) {
+    let xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', file, true);
+    xobj.onreadystatechange = function () {
+      if (xobj.readyState == 4 && xobj.status == "200") {
+        // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+        callback(xobj.responseText);
       }
-      tbody.insertBefore(row, tbody.childNodes[0]);
-    }
-    table.appendChild(tbody);
-  });
+    };
+    xobj.send(null);
+  }
 
-  let tableWrapper = document.createElement("DIV");
-  tableWrapper.classList.add('table-wrapper');
-  tableWrapper.appendChild(table);
+  function printLog(file, filter) {
+    let table = document.createElement("TABLE");
 
-  let headline = document.createElement('H2');
-  headline.innerHTML = getSearchTerm();
-  document.body.appendChild(headline);
+    loadJSON(file, function(response) {
+      // Parse JSON string into object
+      let logObj = JSON.parse(response);
 
-  document.body.appendChild(tableWrapper);
-}
+      // bild table header from the first object set
+      let thead = document.createElement("THEAD");
+      let theadRow = document.createElement("TR");
+      let firstKey = Object.keys(logObj)[0];
+      for (let key in logObj[firstKey]) {
+        let theadCell = document.createElement("TH");
+        theadCell.innerText = key;
+        theadRow.appendChild(theadCell);
+      }
+      thead.appendChild(theadRow);
+      table.appendChild(thead);
 
-function printMenu(logDir) {
-  scanFolder(logDir, function(response) {
-    let index = document.createElement('DIV');
-    index.innerHTML = response;
-    let links = index.querySelectorAll('a');
+      // build table body
+      let tbody = document.createElement("TBODY");
+      for (let serial in logObj) {
 
-    // clean file names
-    let logNames = [];
-    links.forEach( link => {
-      let filePath = link.href;
-      let double = false;
-      if(filePath.includes('.json')) {
-        // get log name from path
-        filePath = filePath.split('/');
-        filePath = filePath[filePath.length - 1];
-        filePath = filePath.split('.json');
-        filePath = filePath[0];
-        // check for doubles
-        logNames.forEach( name => {
-          if(filePath === name) {
-            console.log('included !!!');
-            double = true;
-          }
-        });
-        // add path if new
-        if(!double) {
-          logNames.push(filePath);
+        // filter if there is one
+        if( filter.type && !(filter.type === logObj[serial].type) ) {
+          console.log('skip: ' + logObj[serial].type);
+          continue;
         }
+
+        let row = document.createElement("TR");
+        row.classList.add(logObj[serial].severity);
+        for (let field in logObj[serial]) {
+          let cell = document.createElement("TD");
+          cell.innerText = logObj[serial][field];
+          row.appendChild(cell);
+        }
+        tbody.insertBefore(row, tbody.childNodes[0]);
       }
+      table.appendChild(tbody);
     });
-    buildMenu(logNames);
-  });
-}
+
+    let tableWrapper = document.createElement("DIV");
+    tableWrapper.classList.add('table-wrapper');
+    tableWrapper.appendChild(table);
+
+    let headline = document.createElement('H2');
+    let title = file.split('/');
+    headline.innerHTML = title[title.length - 1].split('.json')[0];
+    document.body.appendChild(headline);
+
+    document.body.appendChild(tableWrapper);
+  }
+
+  function writeOptions(logDir) {
+    scanFolder(logDir, function(response) {
+      let index = document.createElement('DIV');
+      index.innerHTML = response;
+      let links = index.querySelectorAll('a');
+
+      // clean file names
+      let logFiles = [];
+      links.forEach( link => {
+        let filePath = link.href;
+        let double = false;
+        if(filePath.includes('.json')) {
+          // get log name from path
+          filePath = filePath.split('/');
+          filePath = filePath[filePath.length - 1];
+          // check for doubles
+          logFiles.forEach( name => {
+            if(filePath === name) {
+              double = true;
+            }
+          });
+          // add path if new
+          if(!double) {
+            logFiles.push(filePath);
+          }
+        }
+      });
+      let selectBox = document.getElementById('log-files');
+      // build menu links
+      logFiles.forEach( (file, index) => {
+        let option = document.createElement('OPTION');
+        option.innerText = file;
+        option.value = file;
+        selectBox.appendChild(option);
+      });
+    });
+  }
 
 
-( function init() {
-  // specify folder
-  let logDir = 'logs';
-  // get url search term
-  let logFile = logDir + '/' + getSearchTerm() + '.json';
+  ( function init() {
 
-  printMenu(logDir);
-  printLog(logFile);
+    // specify folder
+    const logDir = 'logs';
+
+    // get search terms
+    const params = new URL(location.href).searchParams;
+    const logFile = logDir + '/' + params.get('file');
+    const filter = { 'type':params.get('type'), 'id':888 };
+
+    writeOptions(logDir);
+    printLog(logFile,filter);
+
+  })();
+
+
 
 })();
