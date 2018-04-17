50949b87dd7eea73c67c77d7264dae3a2468d3cf
c3to <c3to@web.de>
2018-04-17 15:45:40 +0200

    sticky header

--- script.js
+++ script.js
@@ -4,7 +4,6 @@
     let fileextension = ".json";
 
     let xobj = new XMLHttpRequest();
-        // xobj.overrideMimeType("html");
     xobj.open('GET', logDir, true);
     xobj.onreadystatechange = function () {
       if (xobj.readyState == 4 && xobj.status == "200") {
@@ -28,92 +27,91 @@
     xobj.send(null);
   }
 
-  function printLog(file, filter) {
-    let table = document.createElement("TABLE");
+  function printLog(filePath, filter) {
+    let log = document.getElementById('log')
+    log.innerHTML = '';
+    let table = document.createElement("table");
 
-    loadJSON(file, function(response) {
+    loadJSON(filePath, function(response) {
       // Parse JSON string into object
       let logObj = JSON.parse(response);
 
       // bild table header from the first object set
-      let thead = document.createElement("THEAD");
-      let theadRow = document.createElement("TR");
+      let thead = document.createElement("thead");
+      let theadRow = document.createElement("tr");
       let firstKey = Object.keys(logObj)[0];
       for (let key in logObj[firstKey]) {
-        let theadCell = document.createElement("TH");
-        theadCell.innerText = key;
-        theadRow.appendChild(theadCell);
+        theadRow.innerHTML += `<th>${key}</th>`;
       }
       thead.appendChild(theadRow);
       table.appendChild(thead);
 
       // build table body
-      let tbody = document.createElement("TBODY");
+      let tbody = document.createElement("tbody");
       for (let serial in logObj) {
 
-        // filter if there is one
-        if( filter.type && !(filter.type === logObj[serial].type) ) {
-          console.log('skip: ' + logObj[serial].type);
-          continue;
-        }
+        // filter
+        let passedFilter = true;
+        filter.forEach( (value, key) => {
+          if(logObj[serial][key] && value != logObj[serial][key]) {
+            passedFilter = false;
+          }
+        });
+        if(!passedFilter) { continue }
 
-        let row = document.createElement("TR");
+        let row = document.createElement("tr");
         row.classList.add(logObj[serial].severity);
         row.addEventListener('click', showDetails);
         for (let field in logObj[serial]) {
-          let cell = document.createElement("TD");
-          cell.innerText = logObj[serial][field];
-          row.appendChild(cell);
+          row.innerHTML += `<td>${logObj[serial][field]}</td>`;
         }
         tbody.insertBefore(row, tbody.childNodes[0]);
       }
       table.appendChild(tbody);
     });
 
-    let tableWrapper = document.createElement("DIV");
-    tableWrapper.classList.add('table-wrapper');
+    let tableWrapper = document.createElement("div");
+    tableWrapper.setAttribute('id', 'table-wrapper');
     tableWrapper.appendChild(table);
 
-    let headline = document.createElement('H2');
+    // create hedline
+    let headline = document.createElement('h2');
     headline.classList.add('title');
-    let title = file.split('/');
+    let title = filePath.split('/');
     headline.innerHTML = title[title.length - 1].split('.json')[0];
-    document.body.appendChild(headline);
+    log.appendChild(headline);
 
-    document.body.appendChild(tableWrapper);
+    log.appendChild(tableWrapper);
+    setTimeout( () => styleThead(), 100 );
   }
 
   function writeOptions(logDir) {
     scanFolder(logDir, function(response) {
-      let index = document.createElement('DIV');
+      let index = document.createElement('div');
       index.innerHTML = response;
       let links = index.querySelectorAll('a');
 
-      // clean file names
-      let logFiles = [];
+      let logFiles = new Set();
+      // fill logFiles
       links.forEach( link => {
         let filePath = link.href;
-        let double = false;
         if(filePath.includes('.json')) {
-          // get log name from path
+
+          // get file name from path
           filePath = filePath.split('/');
           filePath = filePath[filePath.length - 1];
+
           // check for doubles
-          logFiles.forEach( name => {
-            if(filePath === name) {
-              double = true;
-            }
-          });
-          // add path if new
-          if(!double) {
-            logFiles.push(filePath);
+          if(!logFiles.has(filePath)) {
+            logFiles.add(filePath);
           }
         }
       });
-      let selectBox = document.getElementById('log-files');
-      // build menu links
+
+      let selectBox = document.getElementById('filter-file');
+      // build selectbox options
       logFiles.forEach( (file, index) => {
-        let option = document.createElement('OPTION');
+        let option = document.createElement('option');
         option.innerText = file;
         option.value = file;
         selectBox.appendChild(option);
@@ -122,11 +120,10 @@
   }
 
   function showDetails() {
-    let detailsWrapper = document.createElement("DIV");
+    let detailsWrapper = document.createElement("div");
     detailsWrapper.classList.add('details-wrapper');
-    let details = document.createElement("DIV");
+    let details = document.createElement("div");
     detailsWrapper.addEventListener('click', function(e) {
-      // console.log(e.target);
       if(e.target === this) {
         document.body.removeChild(this);
         document.body.classList.remove('details-view');
@@ -134,32 +131,65 @@
     });
     detailsWrapper.appendChild(details);
 
-    let labels = document.getElementsByTagName('TH');
+    let labels = document.getElementsByTagName('th');
 
-    data = this.getElementsByTagName('TD');
+    data = this.getElementsByTagName('td');
     for(let i = 0; i < data.length; i++) {
-      let detail = document.createElement("DIV");
+      let detail = document.createElement("div");
       detail.classList.add('detail');
-      detail.innerHTML += '<span class="label">' + labels[i].innerText + ': </span>';
-      detail.innerHTML += '<span>' + data[i].innerText + '</span>';
+      detail.innerHTML += `<span class="label"> ${labels[i].innerText}: </span>`;
+      detail.innerHTML += `<span class="data"> ${data[i].innerText} </span>`;
       details.appendChild(detail);
     }
     document.body.appendChild(detailsWrapper);
     document.body.classList.add('details-view');
   }
 
-  ( function init() {
+  // style fixed header
+  function styleThead() {
+    let tableWrapper = document.getElementById('table-wrapper');
+    let thead = document.getElementsByTagName('thead');
 
+    let fakeHead = document.createElement('div');
+    fakeHead.style.width = thead[0].offsetWidth;
+
+    tableWrapper.style['max-height'] = (window.innerHeight - tableWrapper.offsetTop - 35);
+    console.dir(tableWrapper);
+
+    let cells = [...thead[0].getElementsByTagName('th')];
+    cells.forEach( (cell) => {
+      fakeHead.innerHTML += `<div style="width:${cell.offsetWidth}px;">${cell.innerText}</div>`;
+    });
+    fakeHead.classList.add('fake-head');
+
+    // make fakeHead sticky
+    tableWrapper.addEventListener('scroll', () => {
+      fakeHead.style.top = tableWrapper.scrollTop;
+    });
+    tableWrapper.insertBefore(fakeHead, tableWrapper.childNodes[0])
+  }
+
+  // initialize
+  ( function init() {
     // specify folder
     const logDir = 'logs';
-
-    // get search terms
-    const params = new URL(location.href).searchParams;
-    const logFile = logDir + '/' + params.get('file');
-    const filter = { 'type':params.get('type'), 'id':888 };
-
     writeOptions(logDir);
-    printLog(logFile,filter);
+    // form filter
+    document.getElementsByTagName('form')[0].addEventListener('submit', function(e) {
+      e.preventDefault();
+      let filePath = `${logDir}/${document.getElementById('filter-file').value}`;
+      let filter = new Map();
+      filter.set('type', document.getElementById('filter-type').value);
+      filter.set('severity', document.getElementById('filter-severity').value);
+
+      // delete empty form fields from the array
+      filter.forEach( (value, key) => {
+        if(value === '') { filter.delete(key); }
+      });
+
+      // print filtered log
+      printLog(filePath, filter);
+    });
 
   })();
 
